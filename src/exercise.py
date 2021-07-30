7dff645ced4e510760a09e30c3060a7e59e8d30b
Justin Turner <jmturner638@gmail.com>
2021-07-30 08:20:39 -0500

    First Try

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,13 @@
 def main():
     #write your code below this line
+    start = int(input("First number?"))
+    finish = int(input("Last number?"))
+    sum = 0
+
+    for i in range(start, finish+1):
+        sum += i
+
+    print("The sum is " + str(sum))
 
 if __name__ == '__main__':
     main()
